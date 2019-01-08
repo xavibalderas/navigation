348413d239e046a8e08b561a9b719017ac38b296
Francisco Javier Balderas Cejudo <frbal5@retchso-xw0007.ikea.com>
2019-01-08 10:48:29 +0100

    pubic

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ const express = require('express');
 const serveStatic = require("serve-static")
 const path = require('path');
 app = express();
-app.use(serveStatic(path.join(__dirname, 'dist')));
+app.use(serveStatic(path.join(__dirname, 'public')));
 const port = process.env.PORT || 80;
 app.listen(port);
