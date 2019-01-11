6ad915ed33050da40da1c2b2d8671ea76a91077d
Francisco Javier Balderas Cejudo <frbal5@retchso-xw0007.ikea.com>
2019-01-11 14:58:03 +0100

    linea

--- src/tools/map.tools.js
+++ src/tools/map.tools.js
@@ -33,7 +33,11 @@ const MapTools = {
     mapwizeMap.setPaintProperty("mapwize_places_fill", 'fill-color', '#FFFFFF');
     mapwizeMap.setPaintProperty("mapwize_places_fill", 'fill-opacity', 0);
     mapwizeMap.setPaintProperty("mapwize_directions_dash", 'line-color', '#FAD23C');
+    mapwizeMap.setPaintProperty("mapwize_directions_dash", 'line-width', 20);
+    //mapwizeMap.setPaintProperty("mapwize_directions_dash", 'line-cap', 'square');
+    mapwizeMap.setPaintProperty("mapwize_directions", 'line-width', 0);
     console.log(mapwizeMap.getLayer('mapwize_places_symbol'));
+    console.log(mapwizeMap);
     store.subscribe((mutation, state) => {
       if (mutation.type === "changePlace"){
         this.showDirections();
