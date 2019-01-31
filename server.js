a918e0b1293cb9326dfc2da346c0aa90ab389941
Xavi <xavibalderas@gmail.com>
2019-01-31 13:01:02 +0100

    working server

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
-const express = require('express');
-const serveStatic = require("serve-static")
-const path = require('path');
-app = express();
-app.use(serveStatic(path.join(__dirname, 'dist')));
-const port = process.env.PORT || 8890;
-app.listen(port);
-//app.get('/en', (req, res) => res.sendFile(path.join(__dirname + '/dist/index.html')));
+const express = require('express')
+const serveStatic = require('serve-static')
+const path = require('path')
+// create the express app
+const app = express()
+// create middleware to handle the serving the app
+app.use("/", serveStatic ( path.join (__dirname, '/dist') ) )
+// Catch all routes and redirect to the index file
+app.get('*', function (req, res) {
+    res.sendFile(__dirname + '/dist/index.html')
+})
+// Create default port to serve the app on
+const port = process.env.PORT || 5001
+app.listen(port)
+// Log to feedback that this is actually running
+console.log('Server started on port ' + port)
